b397105554118c4fa55b6c04b4cff5e3a055445e
Parsa <96871830+Bu64p@users.noreply.github.com>
2022-01-04 13:05:50 +0330

    add guide

--- source/sorting/Merge_sorting.java
+++ source/sorting/Merge_sorting.java
@@ -7,11 +7,11 @@ public class Merge_sorting {
 		this.input_array = input_array;
 		merge_sort(0, input_array.length - 1);
 	}
-
+	// a simple return method
 	public int[] get() {
 		return input_array;
 	}
-
+	// divide arrays by recursive method
 	private void merge_sort(int p, int r) {
 		int q = (int) Math.floor((p + r) / 2);
 		if (p < r) {
@@ -20,16 +20,19 @@ public class Merge_sorting {
 			merge(p, q, r);
 		}
 	}
-
+	// main core of sorting:
 	private void merge(int p, int q, int r) {
 		int n1 = q - p + 1;
 		int n2 = r - q;
+		// make left and tight array of divided index:
 		int[] array1 = new int[n1];
 		int[] array2 = new int[n2];
+		// put unordered elements in right and left arrays:
 		array1 = insert(n1, p);
 		array2 = insert(n2, q + 1);
 		int i = 0, j = 0;
 		int k = p;
+		// check left and right to understand which one is bigger:
 		while (i < n1 && j < n2) {
 			if (array1[i] <= array2[j]) {
 				input_array[k] = array1[i];
@@ -40,18 +43,20 @@ public class Merge_sorting {
 			}
 			k++;
 		}
+		// put missed elements in array:
 		while (i < n1) {
 			input_array[k] = array1[i];
 			i++;
 			k++;
 		}
+		// put missed elements in array:
 		while (j < n2) {
 			input_array[k] = array2[j];
 			j++;
 			k++;
 		}
 	}
-
+	// a simple insert method:
 	private int[] insert(int n, int k) {
 		int[] output = new int[n];
 		for (int i = 0; i < n; ++i) {
